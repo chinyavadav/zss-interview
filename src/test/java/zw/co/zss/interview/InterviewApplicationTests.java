3e4d776cc492bedeb495d347eb6e464c7b2cd7bd
chinyavadav <chinyavadav@gmail.com>
2021-03-16 10:35:05 +0200

    first wireframe commit

---
+++ src/test/java/zw/co/zss/interview/InterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.zss.interview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
